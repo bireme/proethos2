b605695d51fd80cff7b7c85ad006ae5207799539
Moacir Moda <moa.moda@gmail.com>
2016-04-18 19:15:09 -0300

    removing unused files

--- symphony/app/AppKernel.php
+++ symphony/app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
