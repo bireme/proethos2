20c71bb91eeb4b4ff64290945895be19bd3e3c07
Moacir Moda <moa.moda@gmail.com>
2016-04-18 19:20:49 -0300

    generating first bundles

--- symphony/app/AppKernel.php
+++ symphony/app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Proethos2\CoreBundle\Proethos2CoreBundle(),
+            new Proethos2\ModelBundle\Proethos2ModelBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ symphony/src/Proethos2/CoreBundle/Proethos2CoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Proethos2\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Proethos2CoreBundle extends Bundle
+{
+}
