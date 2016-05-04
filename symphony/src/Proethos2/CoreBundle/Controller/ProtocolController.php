be9560c6b76811b7202a7b2f923a0417e12784c3
Moacir Moda <moa.moda@gmail.com>
2016-05-04 14:49:16 -0300

    #95

--- symphony/src/Proethos2/CoreBundle/Controller/ProtocolController.php
+++ symphony/src/Proethos2/CoreBundle/Controller/ProtocolController.php
@@ -336,4 +336,86 @@ class ProtocolController extends Controller
 
         return $output;
     }
+
+    /**
+     * @Route("/protocol/{protocol_id}/initial-committee-review/revisor", name="protocol_initial_committee_review_revisor")
+     * @Template()
+     */
+    public function initCommitteeReviewRevisorAction($protocol_id)
+    {
+        
+        $output = array();
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        $translator = $this->get('translator');
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        
+        $protocol_repository = $em->getRepository('Proethos2ModelBundle:Protocol');
+        $user_repository = $em->getRepository('Proethos2ModelBundle:User');
+        $role_repository = $em->getRepository('Proethos2ModelBundle:Role');
+        $protocol_revision_repository = $em->getRepository('Proethos2ModelBundle:ProtocolRevision');
+        
+        // getting the current submission
+        $protocol = $protocol_repository->find($protocol_id);
+        $submission = $protocol->getMainSubmission();
+        $output['protocol'] = $protocol;
+
+        if (!$protocol or $protocol->getStatus() != "E") {
+            throw $this->createNotFoundException($translator->trans('No protocol found'));
+        }
+        
+        // getting the protocol_revisiion
+        $protocol_revision = $protocol_revision_repository->findOneBy(array("protocol" => $protocol, "member" => $user));
+        $output['protocol_revision'] = $protocol_revision;
+
+        if (!$protocol_revision) {
+            throw $this->createNotFoundException($translator->trans('You are not abble to edit this protocol'));
+        }
+
+        // checking if was a post request
+        if($this->getRequest()->isMethod('POST')) {
+
+            // getting post data
+            $post_data = $request->request->all();
+
+            // only change if is not final revision
+            if(!$protocol_revision->getIsFinalRevision()) {
+
+                // checking required files
+                foreach(array('decision', 'sugestions') as $field) {
+                    if(!isset($post_data[$field]) or empty($post_data[$field])) {
+                        $session->getFlashBag()->add('error', $translator->trans("Field '$field' is required."));
+                        return $output;
+                    }
+                }
+
+                if($post_data['is-final-revision'] == "true") {
+                    $protocol_revision->setIsFinalRevision(true);
+                }
+
+                $protocol_revision->setDecision($post_data['decision']);
+                $protocol_revision->setSocialValue($post_data['social-value']);
+                $protocol_revision->setScientificValidity($post_data['scientific-validity']);
+                $protocol_revision->setFairParticipantSelection($post_data['fair-participant-selection']);
+                $protocol_revision->setFavorableBalance($post_data['favorable-balance']);
+                $protocol_revision->setInformedConsent($post_data['informed-consent']);
+                $protocol_revision->setRespectForParticipants($post_data['respect-for-participants']);
+                $protocol_revision->setOtherComments($post_data['other-comments']);
+                $protocol_revision->setSugestions($post_data['sugestions']);
+                
+                $protocol_revision->setAnswered(true);
+
+                $em->persist($protocol_revision);
+                $em->flush();
+
+                $session->getFlashBag()->add('success', $translator->trans("Fields answered with success!"));
+                return $this->redirectToRoute('protocol_initial_committee_review_revisor', array('protocol_id' => $protocol->getId()), 301);
+            }
+
+        }
+
+        return $output;
+    }
 }
--- symphony/src/Proethos2/ModelBundle/Entity/ProtocolRevision.php
+++ symphony/src/Proethos2/ModelBundle/Entity/ProtocolRevision.php
@@ -38,6 +38,60 @@ class ProtocolRevision extends Base
     private $protocol;
 
     /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_final_revision = false;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="decision", type="string", length=255, nullable=true)
+     */
+    private $decision;   
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $social_value;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $scientific_validity;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $fair_participant_selection;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $favorable_balance;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $informed_consent;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $respect_for_participants;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $other_comments;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="sugestions", type="string", length=255, nullable=true)
+     */
+    private $sugestions;   
+
+    /**
      * Get id
      *
      * @return integer
@@ -117,5 +171,245 @@ class ProtocolRevision extends Base
     public function getProtocol()
     {
         return $this->protocol;
+    }
+
+    /**
+     * Set isFinalRevision
+     *
+     * @param boolean $isFinalRevision
+     *
+     * @return ProtocolRevision
+     */
+    public function setIsFinalRevision($isFinalRevision)
+    {
+        $this->is_final_revision = $isFinalRevision;
+
+        return $this;
+    }
+
+    /**
+     * Get isFinalRevision
+     *
+     * @return boolean
+     */
+    public function getIsFinalRevision()
+    {
+        return $this->is_final_revision;
+    }
+
+    /**
+     * Set decision
+     *
+     * @param string $decision
+     *
+     * @return ProtocolRevision
+     */
+    public function setDecision($decision)
+    {
+        $this->decision = $decision;
+
+        return $this;
+    }
+
+    /**
+     * Get decision
+     *
+     * @return string
+     */
+    public function getDecision()
+    {
+        return $this->decision;
+    }
+
+    /**
+     * Set socialValue
+     *
+     * @param string $socialValue
+     *
+     * @return ProtocolRevision
+     */
+    public function setSocialValue($socialValue)
+    {
+        $this->social_value = $socialValue;
+
+        return $this;
+    }
+
+    /**
+     * Get socialValue
+     *
+     * @return string
+     */
+    public function getSocialValue()
+    {
+        return $this->social_value;
+    }
+
+    /**
+     * Set scientificValidity
+     *
+     * @param string $scientificValidity
+     *
+     * @return ProtocolRevision
+     */
+    public function setScientificValidity($scientificValidity)
+    {
+        $this->scientific_validity = $scientificValidity;
+
+        return $this;
+    }
+
+    /**
+     * Get scientificValidity
+     *
+     * @return string
+     */
+    public function getScientificValidity()
+    {
+        return $this->scientific_validity;
+    }
+
+    /**
+     * Set fairParticipantSelection
+     *
+     * @param string $fairParticipantSelection
+     *
+     * @return ProtocolRevision
+     */
+    public function setFairParticipantSelection($fairParticipantSelection)
+    {
+        $this->fair_participant_selection = $fairParticipantSelection;
+
+        return $this;
+    }
+
+    /**
+     * Get fairParticipantSelection
+     *
+     * @return string
+     */
+    public function getFairParticipantSelection()
+    {
+        return $this->fair_participant_selection;
+    }
+
+    /**
+     * Set favorableBalance
+     *
+     * @param string $favorableBalance
+     *
+     * @return ProtocolRevision
+     */
+    public function setFavorableBalance($favorableBalance)
+    {
+        $this->favorable_balance = $favorableBalance;
+
+        return $this;
+    }
+
+    /**
+     * Get favorableBalance
+     *
+     * @return string
+     */
+    public function getFavorableBalance()
+    {
+        return $this->favorable_balance;
+    }
+
+    /**
+     * Set informedConsent
+     *
+     * @param string $informedConsent
+     *
+     * @return ProtocolRevision
+     */
+    public function setInformedConsent($informedConsent)
+    {
+        $this->informed_consent = $informedConsent;
+
+        return $this;
+    }
+
+    /**
+     * Get informedConsent
+     *
+     * @return string
+     */
+    public function getInformedConsent()
+    {
+        return $this->informed_consent;
+    }
+
+    /**
+     * Set respectForParticipants
+     *
+     * @param string $respectForParticipants
+     *
+     * @return ProtocolRevision
+     */
+    public function setRespectForParticipants($respectForParticipants)
+    {
+        $this->respect_for_participants = $respectForParticipants;
+
+        return $this;
+    }
+
+    /**
+     * Get respectForParticipants
+     *
+     * @return string
+     */
+    public function getRespectForParticipants()
+    {
+        return $this->respect_for_participants;
+    }
+
+    /**
+     * Set otherComments
+     *
+     * @param string $otherComments
+     *
+     * @return ProtocolRevision
+     */
+    public function setOtherComments($otherComments)
+    {
+        $this->other_comments = $otherComments;
+
+        return $this;
+    }
+
+    /**
+     * Get otherComments
+     *
+     * @return string
+     */
+    public function getOtherComments()
+    {
+        return $this->other_comments;
+    }
+
+    /**
+     * Set sugestions
+     *
+     * @param string $sugestions
+     *
+     * @return ProtocolRevision
+     */
+    public function setSugestions($sugestions)
+    {
+        $this->sugestions = $sugestions;
+
+        return $this;
+    }
+
+    /**
+     * Get sugestions
+     *
+     * @return string
+     */
+    public function getSugestions()
+    {
+        return $this->sugestions;
     }
 }
