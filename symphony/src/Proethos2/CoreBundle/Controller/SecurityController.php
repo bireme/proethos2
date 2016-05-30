2aea568ee9ccf264364e02e0048362f07d088464
Moacir Moda <moa.moda@gmail.com>
2016-05-30 12:38:52 -0300

    #134

---
+++ symphony/src/Proethos2/CoreBundle/Controller/SecurityController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Proethos2\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login_route")
+     * @Template()
+     */
+    public function loginAction()
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            );
+    }
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheckAction()
+    {
+        // this controller will not be executed,
+        // as the route is handled by the Security system
+    }
+
+    /**
+     * @Route("/logout", name="logout_route")
+     */
+    public function logoutAction()
+    {
+        // this controller will not be executed,
+        // as the route is handled by the Security system
+    }
+
+
+}
