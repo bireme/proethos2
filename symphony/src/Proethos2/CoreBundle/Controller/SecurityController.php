b6baf85dcc5ad3b4cf5cc46a2dd5ff4c74c51872
Moacir Moda <moa.moda@gmail.com>
2016-05-30 14:41:10 -0300

    #131 - making change password modal

--- symphony/src/Proethos2/CoreBundle/Controller/SecurityController.php
+++ symphony/src/Proethos2/CoreBundle/Controller/SecurityController.php
@@ -50,5 +50,77 @@ class SecurityController extends Controller
         // as the route is handled by the Security system
     }
 
+    /**
+     * @Route("/account/change_password", name="security_change_password")
+     * @Template()
+     */
+    public function changePasswordAction()
+    {
+        $output = array();
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        $translator = $this->get('translator');
+        $em = $this->getDoctrine()->getManager();
 
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+
+        // checking if was a post request
+        if($this->getRequest()->isMethod('POST')) {
+
+            // getting post data
+            $post_data = $request->request->all();
+            
+            // checking required fields
+            foreach(array('change-password', 'password-confirm') as $field) {   
+                if(!isset($post_data[$field]) or empty($post_data[$field])) {
+                    $session->getFlashBag()->add('error', $translator->trans("Field '$field' is required."));
+                    return $this->redirectToRoute('home', array(), 301);
+                }
+            }
+
+            if($post_data['change-password'] != $post_data['password-confirm']) {
+                $session->getFlashBag()->add('error', $translator->trans("Passwords doesn't match."));
+                return $this->redirectToRoute('home', array(), 301);
+            }
+
+            $encoderFactory = $this->get('security.encoder_factory');
+            $encoder = $encoderFactory->getEncoder($user);
+            $salt = $user->getSalt(); // this should be different for every user
+            $password = $encoder->encodePassword($post_data['change-password'], $salt);
+            $user->setPassword($password);
+
+            $em->persist($user);
+            $em->flush();
+
+            $session->getFlashBag()->add('success', $translator->trans("Password changed with success."));
+            return $this->redirectToRoute('home', array(), 301);
+
+
+            // if(isset($post_data['new-meeting-date'])) {
+
+                
+            //     // checking required fields
+            //     foreach(array('new-meeting-date', 'new-meeting-subject', 'new-meeting-content') as $field) {   
+            //         if(!isset($post_data[$field]) or empty($post_data[$field])) {
+            //             $session->getFlashBag()->add('error', $translator->trans("Field '$field' is required."));
+            //             return $output;
+            //         }
+            //     }
+
+            //     $meeting = new Meeting();
+            //     $meeting->setDate(new \DateTime($post_data['new-meeting-date']));
+            //     $meeting->setSubject($post_data['new-meeting-subject']);
+            //     $meeting->setContent($post_data['new-meeting-content']);
+
+            //     $em->persist($meeting);
+            //     $em->flush();
+
+            //     $session->getFlashBag()->add('success', $translator->trans("Meeting created with success."));
+            //     return $this->redirectToRoute('crud_committee_meeting_list', array(), 301);
+            // }
+
+        }
+
+        return $output;
+    }
 }
---
+++ symphony/src/Proethos2/CoreBundle/Tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Proethos2\CoreBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    var $client;
+    var $_em;
+
+    public function setUp()
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $this->_em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        
+    }
+
+    public function login() {
+        return $this->client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'admin',
+            'PHP_AUTH_PW'   => 'admin'
+        ));
+    }
+
+    public function testLogin()
+    {
+        $client = static::createClient();
+        $route = $client->getContainer()->get('router')->generate('login_route', array(), false);
+        
+        $client->request('GET', $route);
+        $this->assertNotEquals(301, $client->getResponse()->getStatusCode());
+    }
+
+    public function testChangePassword()
+    {
+        $client = $this->login();
+        
+        $route = $client->getContainer()->get('router')->generate('security_change_password', array(), false);
+        
+        $client->request('GET', $route);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
