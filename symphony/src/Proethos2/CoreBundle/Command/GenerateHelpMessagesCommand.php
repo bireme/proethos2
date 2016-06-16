c789665136f50d616eeb997d0ba118d387d4e689
Moacir Moda <moa.moda@gmail.com>
2016-06-16 11:18:12 -0300

    #186 generating help entity and making a tool that generate and associate helps

---
+++ symphony/src/Proethos2/CoreBundle/Command/GenerateHelpMessagesCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Proethos2\CoreBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Proethos2\ModelBundle\Entity\Help;
+
+class GenerateHelpMessagesCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('proethos2:generate-help-messages')
+            ->setDescription('Generate help message')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine')->getEntityManager();
+        $root_dir = $this->getContainer()->get('kernel')->getRootDir();
+
+        // iterates finding all the .twig files
+        $it = new \RecursiveDirectoryIterator($root_dir . '/../src');
+        foreach(new \RecursiveIteratorIterator($it) as $filepath => $object) {
+            if(strpos($filepath, ".twig") > -1) {
+                
+                // get file contents and set the new content
+                $content = file_get_contents($filepath);
+                $new_content = "";
+
+                $count = 0;
+                $content_by_lines = explode("\n", $content);
+                foreach($content_by_lines as $line) {
+                    if(strpos($line, "#modal-help") > -1) {
+
+                        // if doesnt mapped, associate an id to this help link
+                        if(strpos($line, "data-help-id") === false) {
+
+                            // create a new help text
+                            $help = new Help();
+                            $em->persist($help);
+                            $em->flush();
+
+                            $line = str_replace('data-target="#modal-help"', 'data-help-id="'. $help->getId() .'" data-target="#modal-help"', $line);
+                            $output->writeln(trim($line));
+                        }
+
+                    }
+
+                    // concat the new line on the new content.
+                    $count += 1;
+                    $new_content .= $line;
+                    // If is the last line, doesn't print the break line
+                    if(count($content_by_lines) > $count) {
+                        $new_content .= "\n";
+                    }
+                }
+
+                // write the new file
+                file_put_contents($filepath, $new_content);
+            }
+        }
+    }
+}
---
+++ symphony/src/Proethos2/ModelBundle/Entity/Help.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Proethos2\ModelBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Help
+ *
+ * @ORM\Table(name="help")
+ * @ORM\Entity
+ */
+class Help extends Base
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $status = false;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $message;
+
+    public function __toString() {
+        return $this->getMessage();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set status
+     *
+     * @param boolean $status
+     *
+     * @return Help
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return boolean
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return Help
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+}
