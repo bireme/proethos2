98f87ed8a4f8228e12dcc99c49dc6ff33cf2996c
Moacir Moda <moa.moda@gmail.com>
2016-06-20 11:52:28 -0300

    generating initial data with replace command

--- symphony/src/Proethos2/CoreBundle/Command/GenerateDatabaseInitialDataCommand.php
+++ symphony/src/Proethos2/CoreBundle/Command/GenerateDatabaseInitialDataCommand.php
@@ -49,7 +49,7 @@ class GenerateDatabaseInitialDataCommand extends ContainerAwareCommand
         }
 
         foreach($tables as $table) {
-            $command = "mysqldump -h $host -u$database_user -p$database_password $database_name --no-create-info -t $table";
+            $command = "mysqldump -h $host -u$database_user -p$database_password $database_name --replace --no-create-info -t $table";
             $command .= " > $fixtures_dir/data_$table.sql";
             exec($command);
         }
