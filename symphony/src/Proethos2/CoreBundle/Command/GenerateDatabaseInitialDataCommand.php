a483c4faec20917bc37e5c5879f5d060a4ed8f93
Moacir Moda <moa.moda@gmail.com>
2016-06-21 10:22:54 -0300

    removing unused entity

--- symphony/src/Proethos2/CoreBundle/Command/GenerateDatabaseInitialDataCommand.php
+++ symphony/src/Proethos2/CoreBundle/Command/GenerateDatabaseInitialDataCommand.php
@@ -38,7 +38,6 @@ class GenerateDatabaseInitialDataCommand extends ContainerAwareCommand
             'list_monitoring_action',
             'list_recruitment_status',
             'list_role',
-            'list_upload_type',
             'upload_type',
             'help',
         );
