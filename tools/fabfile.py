6b9aea8e25b06a59177d202449c786d52dc3e7a1
Moacir Moda <moa.moda@gmail.com>
2016-07-08 13:22:29 -0300

    updating fabfile with tests

--- tools/fabfile.py
+++ tools/fabfile.py
@@ -1,4 +1,5 @@
 #!coding:utf-8
+import os
 from fabric.api import env, local, settings, abort, run, cd
 from fabric.operations import local, put, sudo, get
 from fabric.context_managers import prefix
@@ -8,7 +9,8 @@ from fabenv import *
 env.use_ssh_config = True
 
 def update():
-
+    local('phpunit -c /home/moa/project/proethos2/git/symphony/app')
+    
     with prefix(". %s" % env.env_script):
         with cd(env.path):
             run("git pull origin master")
@@ -18,4 +20,3 @@ def update():
             run("php app/console doctrine:schema:update --force")
             run("php app/console proethos2:load-database-initial-data")
             run("php app/console cache:clear --env=prod")
-            
