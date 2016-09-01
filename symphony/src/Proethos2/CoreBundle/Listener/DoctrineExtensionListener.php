3e2edda6b6f259fd8c1d7924ca554a66d5755e94
Moacir Moda <moa.moda@gmail.com>
2016-09-01 10:17:40 -0300

    #269 adding headers in php files

--- symphony/src/Proethos2/CoreBundle/Listener/DoctrineExtensionListener.php
+++ symphony/src/Proethos2/CoreBundle/Listener/DoctrineExtensionListener.php
@@ -1,5 +1,20 @@
 <?php
 
+// This file is part of the ProEthos Software. 
+// 
+// Copyright 2013, PAHO. All rights reserved. You can redistribute it and/or modify
+// ProEthos under the terms of the ProEthos License as published by PAHO, which
+// restricts commercial use of the Software. 
+// 
+// ProEthos is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the ProEthos License for more details. 
+// 
+// You should have received a copy of the ProEthos License along with the ProEthos
+// Software. If not, see
+// https://github.com/bireme/proethos2/blob/master/doc/license.md
+
+
 namespace Proethos2\CoreBundle\Listener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
