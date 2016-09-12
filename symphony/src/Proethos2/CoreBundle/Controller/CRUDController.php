c10621ef9e977c9c1975d2ec1da42801676867cc
Moacir Moda <moa.moda@gmail.com>
2016-09-12 15:56:11 -0300

    removing required fields from configuration module (#283)

--- symphony/src/Proethos2/CoreBundle/Controller/CRUDController.php
+++ symphony/src/Proethos2/CoreBundle/Controller/CRUDController.php
@@ -1371,15 +1371,6 @@ class CRUDController extends Controller
             // getting post data
             $post_data = $request->request->all();
             
-            // checking required files
-            foreach(array('configuration-value') as $field) {
-                
-                if(!isset($post_data[$field]) or empty($post_data[$field])) {
-                    $session->getFlashBag()->add('error', $translator->trans("Field '%field%' is required.", array("%field%" => $field)));
-                    return $output;
-                }
-            }
-
             $configuration->setValue($post_data['configuration-value']);
             
             $em->persist($configuration);
