72343c976cc1ce6b577a9554a21d833247a7e291
Moacir Moda <moa.moda@gmail.com>
2016-09-13 08:35:18 -0300

    updating in tool that remove state new from messages

--- tools/remove-reference-file-lines-from-messages.sh
+++ tools/remove-reference-file-lines-from-messages.sh
@@ -4,4 +4,7 @@
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$DIR/../symphony"
+# removing lines unused
 find app/ -type f -name "*.xlf" -exec sed -i '/jms:reference-file line/d' {} \;
+
+find app/ -type f -name "*.xlf" -exec sed -i 's/ state\=\"new\"//g' {} \;
