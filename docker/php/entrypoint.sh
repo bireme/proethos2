62bb30a7baa7c7173bff52980e8b0e80d67e2343
Moacir Moda <moa.moda@gmail.com>
2016-09-26 12:43:34 -0300

    adding some dependencies on docker files

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -2,10 +2,22 @@
 
 cd /tmp
 
+# libs
 apt-get update
-apt-get install -y --force-yes mysql-client wget
+apt-get install -y --force-yes mysql-client wget libfreetype6-dev libjpeg62-turbo-dev libmcrypt-dev libpng12-dev
 
 docker-php-ext-install mysql mysqli pdo pdo_mysql
+# php extensions
+docker-php-ext-install -j$(nproc) iconv mcrypt
+docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
+docker-php-ext-install -j$(nproc) gd
+
+# phpunit
+wget https://phar.phpunit.de/phpunit-4.8.2.phar
+chmod +x phpunit-4.8.2.phar
+mv phpunit-4.8.2.phar /usr/bin/phpunit
+phpunit --version
+
 # composer
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
