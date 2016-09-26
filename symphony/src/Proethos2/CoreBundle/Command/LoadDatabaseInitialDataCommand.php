3a390c4497d0abf2bc74689e500739436190ea9b
Moacir Moda <moa.moda@gmail.com>
2016-09-26 12:43:11 -0300

    including configuration in load initial data

--- symphony/src/Proethos2/CoreBundle/Command/LoadDatabaseInitialDataCommand.php
+++ symphony/src/Proethos2/CoreBundle/Command/LoadDatabaseInitialDataCommand.php
@@ -1,15 +1,15 @@
 <?php
 
-// This file is part of the ProEthos Software. 
-// 
+// This file is part of the ProEthos Software.
+//
 // Copyright 2013, PAHO. All rights reserved. You can redistribute it and/or modify
 // ProEthos under the terms of the ProEthos License as published by PAHO, which
-// restricts commercial use of the Software. 
-// 
+// restricts commercial use of the Software.
+//
 // ProEthos is distributed in the hope that it will be useful, but WITHOUT ANY
 // WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-// PARTICULAR PURPOSE. See the ProEthos License for more details. 
-// 
+// PARTICULAR PURPOSE. See the ProEthos License for more details.
+//
 // You should have received a copy of the ProEthos License along with the ProEthos
 // Software. If not, see
 // https://github.com/bireme/proethos2/blob/master/doc/license.md
@@ -45,6 +45,7 @@ class LoadDatabaseInitialDataCommand extends ContainerAwareCommand
         $database_password = $this->getContainer()->getParameter('database_password');
 
         $tables = array(
+            'configuration',
             'list_clinical_trial_name',
             'list_country',
             'list_gender',
