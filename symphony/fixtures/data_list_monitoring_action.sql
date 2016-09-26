54d6d3b8dc617cff2c4a911403ccdca4d0e0e23c
Moacir Moda <moa.moda@gmail.com>
2016-09-26 14:33:02 -0300

    updating fixtures

--- symphony/fixtures/data_list_monitoring_action.sql
+++ symphony/fixtures/data_list_monitoring_action.sql
@@ -1,4 +1,4 @@
-REPLACE INTO `list_monitoring_action` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (1,'1970-01-01 00:00:00','1970-01-01 00:00:00','Submit an amendment ','submit-an-amendment',1);
+REPLACE INTO `list_monitoring_action` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (1,'1970-01-01 00:00:00','1970-01-01 00:00:00','Submit an amendment','submit-an-amendment',1);
 REPLACE INTO `list_monitoring_action` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (2,'1970-01-01 00:00:00','1970-01-01 00:00:00','Adverse event report','adverse-event-report',1);
 REPLACE INTO `list_monitoring_action` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (3,'1970-01-01 00:00:00','1970-01-01 00:00:00','Conclusion of study','conclusion-of-study',1);
 REPLACE INTO `list_monitoring_action` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (4,'1970-01-01 00:00:00','1970-01-01 00:00:00','Follow-up report','follow-up-report',1);
