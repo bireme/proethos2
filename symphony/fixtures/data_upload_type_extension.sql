b29e0ee7d01dc80c1a90d9195d9f3753677263e2
Moacir Moda <moa.moda@gmail.com>
2016-09-26 13:15:37 -0300

    fixing some tests and add others

---
+++ symphony/fixtures/data_upload_type_extension.sql
@@ -0,0 +1,4 @@
+REPLACE INTO `upload_type_extension` VALUES (1,'2016-08-26 12:29:31','2016-08-26 12:29:31','.pdf',1);
+REPLACE INTO `upload_type_extension` VALUES (2,'2016-08-26 12:29:43','2016-08-26 12:29:43','.docx',1);
+REPLACE INTO `upload_type_extension` VALUES (3,'2016-08-26 12:29:54','2016-08-26 12:29:54','.doc',1);
+REPLACE INTO `upload_type_extension` VALUES (4,'2016-08-26 12:34:03','2016-08-26 12:34:03','.xlsx',1);
