da9c954aca4199e95adfea5de6896decc6076e63
Wilson da Silva Moura <wilsonsmoura@gmail.com>
2019-09-06 14:37:55 -0300

    Fixed #440 -- Update data_user_role.sql

--- symphony/fixtures/data_user_role.sql
+++ symphony/fixtures/data_user_role.sql
@@ -1,3 +1,5 @@
+REPLACE INTO `user_role` (`user_id`, `role_id`) VALUES (1,1);
+REPLACE INTO `user_role` (`user_id`, `role_id`) VALUES (1,2);
 REPLACE INTO `user_role` (`user_id`, `role_id`) VALUES (1,3);
 REPLACE INTO `user_role` (`user_id`, `role_id`) VALUES (1,4);
 REPLACE INTO `user_role` (`user_id`, `role_id`) VALUES (1,5);
