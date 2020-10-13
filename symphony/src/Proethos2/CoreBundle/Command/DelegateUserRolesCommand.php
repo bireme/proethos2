f4c93fad9456cad5cb560efa7ebd62d355676f34
wilsonmoura <wilsonsmoura@gmail.com>
2020-10-13 14:04:43 -0300

    Fixed #467 -- Oauth2 authentication (Azure AD)

---
+++ symphony/src/Proethos2/CoreBundle/Command/DelegateUserRolesCommand.php
@@ -0,0 +1,82 @@
+<?php
+
+// This file is part of the ProEthos Software.
+//
+// Copyright 2013, PAHO. All rights reserved. You can redistribute it and/or modify
+// ProEthos under the terms of the ProEthos License as published by PAHO, which
+// restricts commercial use of the Software.
+//
+// ProEthos is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the ProEthos License for more details.
+//
+// You should have received a copy of the ProEthos License along with the ProEthos
+// Software. If not, see
+// https://github.com/bireme/proethos2/blob/master/LICENSE.txt
+
+
+namespace Proethos2\CoreBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Proethos2\CoreBundle\Util\Util;
+
+
+class DelegateUserRolesCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('proethos2:delegate-user-roles')
+            ->setDescription('Delegate roles for any existing user on the ProEthos system')
+            ->setHelp('Usage: php app/console proethos2:delegate-user-roles <email> <roles>')
+            ->addArgument('email', InputArgument::REQUIRED, 'Insert the user email.')
+            ->addArgument('roles', InputArgument::REQUIRED, 'Insert the user roles ID (comma-separated): 1-investigator|2-secretary|3-member-of-committee|4-member-ad-hoc|5-administrator (e.g. 1,2,3,4,5)')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $container = $this->getContainer();
+        $doctrine = $container->get('doctrine');
+        $translator = $container->get('translator');
+
+        $em = $doctrine->getManager();
+        $util = new Util($container, $doctrine);
+        $role_repository = $em->getRepository('Proethos2ModelBundle:Role');
+
+        $user_roles = array(
+            'investigator'        => 1,
+            'secretary'           => 2,
+            'member-of-committee' => 3,
+            'member-ad-hoc'       => 4,
+            'administrator'       => 5            
+        );
+
+        $email = $input->getArgument('email');
+        $list_roles = explode(',', $input->getArgument('roles'));
+        $roles = array_intersect($user_roles, $list_roles);
+
+        $user_repository = $em->getRepository('Proethos2ModelBundle:User');
+        $user = $user_repository->findOneBy(array('email' => $email));
+
+        if ( $user ) {
+            foreach ($roles as $role => $role_id) {
+                $user->addProethos2Role($role_repository->findOneBy(array('slug' => $role)));
+            }
+
+            $em->persist($user);
+            $em->flush();
+
+            $output_roles = implode('|', array_keys($roles));
+            $output->writeln(sprintf("\n[DONE] user roles for '%s': %s\n", $email, $output_roles));
+        } else {
+            $output->writeln(sprintf("\n[ERROR] user email '%s' not found.\n", $email));
+        }
+    }
+}
--- symphony/src/Proethos2/CoreBundle/Controller/SecurityController.php
+++ symphony/src/Proethos2/CoreBundle/Controller/SecurityController.php
@@ -19,20 +19,38 @@ namespace Proethos2\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use TheNetworg\OAuth2\Client\Provider\Azure;
 
 use Proethos2\ModelBundle\Entity\User;
 use Proethos2\CoreBundle\Util\Util;
 
+use Cocur\Slugify\Slugify;
+
+
 class SecurityController extends Controller
 {
+    public function __construct()
+    {
+        global $kernel;
+        $env_dir = dirname($kernel->getRootDir());
+
+        // load enviroment variables
+        $dotenv = new Dotenv();
+        $dotenv->load($env_dir.'/.env');
+    }
+
     /**
      * @Route("/login", name="login_route")
      * @Template()
      */
-    public function loginAction()
+    public function loginAction(Request $request)
     {
         $util = new Util($this->container, $this->getDoctrine());
         
@@ -48,14 +66,161 @@ class SecurityController extends Controller
         $committee_description = $util->getConfiguration("committee.description");
         $committee_logourl = $util->getConfiguration("committee.logourl");
 
-        return array(
-                'last_username' => $lastUsername,
-                'error'         => $error,
-                
-                'committee_name' => $committee_name,
-                'committee_description' => $committee_description,
-                'committee_logourl' => $committee_logourl,
-            );
+        $auth_type = $this->container->getParameter('auth_type');
+
+        $output = array(
+            'last_username'         => $lastUsername,
+            'error'                 => $error,
+            'auth_type'             => $auth_type,
+            'committee_name'        => $committee_name,
+            'committee_description' => $committee_description,
+            'committee_logourl'     => $committee_logourl,
+        );
+
+        if ( 'oauth2' == $auth_type ) {
+            $router = $this->container->get('router');
+            $redirect = $router->generate('login_route', array(), true);
+            $azureClientId = ( $_ENV['AZURE_CLIENT_ID'] ) ? $_ENV['AZURE_CLIENT_ID'] : '';
+            $azureClientSecret = ( $_ENV['AZURE_CLIENT_SECRET'] ) ? $_ENV['AZURE_CLIENT_SECRET'] : '';
+
+            $provider = new Azure([
+                'clientId'     => $azureClientId,
+                'clientSecret' => $azureClientSecret,
+                'redirectUri'  => $redirect
+            ]);
+
+            // Set to use v2 API, skip the line or set the value to Azure::ENDPOINT_VERSION_1_0 if willing to use v1 API
+            $provider->defaultEndPointVersion = Azure::ENDPOINT_VERSION_2_0;
+
+            $baseGraphUri = $provider->getRootMicrosoftGraphUri(null);
+            $provider->scope = 'openid profile email offline_access ' . $baseGraphUri . '/User.Read';
+
+            $oauth2_code = $request->query->get('code');
+            $oauth2_state = $request->query->get('state');
+            $session_state = $this->get('session')->get('OAuth2.state');
+
+            if (isset($oauth2_code) && isset($session_state) && isset($oauth2_state)) {
+                if ($oauth2_state == $session_state) {
+                    $this->get('session')->remove('OAuth2.state');
+
+                    // Try to get an access token (using the authorization code grant)
+                    /** @var AccessToken $token */
+                    $token = $provider->getAccessToken('authorization_code', [
+                        'scope' => $provider->scope,
+                        'code' => $_GET['code'],
+                    ]);
+
+                    // Verify token
+                    //$token->getToken()
+                    $data = $provider->get($provider->getRootMicrosoftGraphUri($token) . '/v1.0/me', $token);
+
+                    if ( !$data ) {
+                        $output['error'] = $translator->trans("Invalid login.");
+                        return $output;
+                    }
+
+                    // Example of how to obtain an user:
+                    $em = $this->getDoctrine()->getManager();
+                    $user_repository = $em->getRepository('Proethos2ModelBundle:User');
+                    $user = $user_repository->findOneBy(array('email' => $data['mail']));
+
+                    if ( !$user ) { // create user
+                        $em = $this->getDoctrine()->getManager();
+                        $translator = $this->get('translator');
+                        $locale = $request->getSession()->get('_locale');
+                        $baseurl = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath();
+                        $trans_repository = $em->getRepository('Gedmo\\Translatable\\Entity\\Translation');
+                        $help_repository = $em->getRepository('Proethos2ModelBundle:Help');
+                        // $role_repository = $em->getRepository('Proethos2ModelBundle:Role');
+
+                        $slugify = new Slugify();
+                        $givenName = explode(' ', $data['givenName']);
+                        $surname = explode(' ', $data['surname']);
+                        $username = $slugify->slugify(array_pop($surname).$givenName[0]);
+                        $username = substr($username, 0, 8);
+                        
+                        $user = $user_repository->findOneBy(array('username' => $username));
+                        if ( $user ) {
+                            $username = $slugify->slugify($givenName[0].array_pop($surname));
+                        }
+
+                        $user = new User();
+                        $user->setName($data['givenName'].' '.$data['surname']);
+                        $user->setUsername($username);
+                        $user->setEmail($data['mail']);
+                        $user->setFirstAccess(false);
+                        $user->setIsActive(false);
+
+                        // adding user role
+                        // $user->addProethos2Role($role_repository->findOneBy(array('slug' => 'investigator')));
+
+                        $encoderFactory = $this->get('security.encoder_factory');
+                        $encoder = $encoderFactory->getEncoder($user);
+                        $salt = $user->getSalt(); // this should be different for every user
+                        $password = $encoder->encodePassword(md5(date("YmdHis")), $salt);
+                        $user->setPassword($password);
+
+                        $user->cleanHashcode();
+
+                        $help = $help_repository->find(208);
+                        $translations = $trans_repository->findTranslations($help);
+                        $text = $translations[$locale];
+                        $body = $text['message'];
+                        $body = str_replace("%home_url%", $baseurl, $body);
+                        $body = str_replace("\r\n", "<br />", $body);
+                        $body .= "<br /><br />";
+
+                        // send email to the secretaries
+                        $secretaries_emails = array();
+                        foreach($user_repository->findAll() as $secretary) {
+                            if(in_array('secretary', $secretary->getRolesSlug())) {
+                                $secretaries_emails[] = $secretary->getEmail();
+                            }
+                        }
+
+                        $message = \Swift_Message::newInstance()
+                        ->setSubject("[proethos2] " . $translator->trans("New user on Proethos2 platform"))
+                        ->setFrom($util->getConfiguration('committee.email'))
+                        ->setTo($secretaries_emails)
+                        ->setBody(
+                            $body
+                            ,   
+                            'text/html'
+                        );
+                        $send = $this->get('mailer')->send($message);
+
+                        $em->persist($user);
+                        $em->flush();
+                    }
+
+                    //Handle getting or creating the user entity likely with a posted form
+                    // The third parameter "main" can change according to the name of your firewall in security.yml
+                    //$token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+                    $token = new UsernamePasswordToken($user, null, 'default', $user->getRoles());
+                    $this->get('security.token_storage')->setToken($token);
+
+                    // If the firewall name is not main, then the set value would be instead:
+                    //$this->get('session')->set('_security_main', serialize($token));
+                    $this->get('session')->set('_security_default', serialize($token));
+                    
+                    // Fire the login event manually
+                    $event = new InteractiveLoginEvent($request, $token);
+                    $this->get("event_dispatcher")->dispatch("security.interactive_login", $event);
+                    
+                    /*
+                     * Now the user is authenticated !!!! 
+                     * Do what you need to do now, like render a view, redirect to route etc.
+                     */
+
+                    return $this->redirectToRoute('default_security_target', array(), 301);
+                } else {
+                    $output['error'] = $translator->trans("Invalid state.");
+                    return $output;
+                }
+            }
+        }
+
+        return $output;
     }
 
     /**
@@ -91,6 +256,39 @@ class SecurityController extends Controller
         }
         
         return $this->redirectToRoute('crud_investigator_protocol_list', array(), 301);
+    }
+
+    /**
+     * @Route("/login/oauth2", name="login_oauth2")
+     */
+    public function loginAzureAction()
+    {
+        $auth_type = $this->container->getParameter('auth_type');
+
+        if ( 'oauth2' == $auth_type ) {
+            $router = $this->container->get('router');
+            $redirect = $router->generate('login_route', array(), true);
+            $azureClientId = ( $_ENV['AZURE_CLIENT_ID'] ) ? $_ENV['AZURE_CLIENT_ID'] : '';
+            $azureClientSecret = ( $_ENV['AZURE_CLIENT_SECRET'] ) ? $_ENV['AZURE_CLIENT_SECRET'] : '';
+            
+            $provider = new Azure([
+                'clientId'     => $azureClientId,
+                'clientSecret' => $azureClientSecret,
+                'redirectUri'  => $redirect
+            ]);
+
+            // Set to use v2 API, skip the line or set the value to Azure::ENDPOINT_VERSION_1_0 if willing to use v1 API
+            $provider->defaultEndPointVersion = Azure::ENDPOINT_VERSION_2_0;
+
+            $baseGraphUri = $provider->getRootMicrosoftGraphUri(null);
+            $provider->scope = 'openid profile email offline_access ' . $baseGraphUri . '/User.Read';
+            $authorizationUrl = $provider->getAuthorizationUrl(['scope' => $provider->scope]);
+            $this->get('session')->set('OAuth2.state', $provider->getState());
+            
+            return $this->redirect($authorizationUrl, 301);
+        } else {
+            return $this->redirectToRoute('login_route', array(), 301);
+        }
     }
 
     /**
