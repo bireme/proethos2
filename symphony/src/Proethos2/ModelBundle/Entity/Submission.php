11fef7e0ecf403a0ca68469e97c11d444876810e
wilsonmoura <wilsonsmoura@gmail.com>
2021-02-24 18:19:24 -0300

    Fixed #521 -- Display role and institution fields in team member list

--- symphony/src/Proethos2/ModelBundle/Entity/Submission.php
+++ symphony/src/Proethos2/ModelBundle/Entity/Submission.php
@@ -220,6 +220,7 @@ class Submission extends Base
      * @ORM\JoinColumn(name="gender_id", referencedColumnName="id", onDelete="SET NULL")
      */
     private $gender;
+    
     /**
      * @var integer
      *
