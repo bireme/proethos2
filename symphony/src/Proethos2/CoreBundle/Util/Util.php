89b3dfb41c607bc3b493346859e483d3172a9c7f
wilsonmoura <wilsonsmoura@gmail.com>
2023-10-17 21:14:38 -0300

    #573 -- Editar sujeto en los correos automáticos

--- symphony/src/Proethos2/CoreBundle/Util/Util.php
+++ symphony/src/Proethos2/CoreBundle/Util/Util.php
@@ -37,7 +37,7 @@ class Util {
 
             if($configuration and !empty($configuration->getValue())) {
                 $value = $configuration->getValue();
-                $committee_contact = explode("\n", $value);
+                $committee_contact = preg_split('/\r\n|\r|\n/', $value);
                 
                 if( $committee_contact && count($committee_contact) == 2 ) {
                     return $committee_contact[1];
@@ -56,7 +56,7 @@ class Util {
             if($configuration and !empty($configuration->getValue())) {
                 if ( 'committee.email' == $key ) {
                     $value = $configuration->getValue();
-                    $committee_email = explode("\n", $value);
+                    $committee_email = preg_split('/\r\n|\r|\n/', $value);
                     
                     if( $committee_email ) {
                         return $committee_email[0];
