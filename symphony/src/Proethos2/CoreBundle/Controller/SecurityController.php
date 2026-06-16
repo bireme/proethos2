633d3b769ab3817c7765eb56409a61933ffb9e2e
gabrielszz <50212683+gabrielszz@users.noreply.github.com>
2026-06-16 09:45:10 -0300

    Update SecurityController.php

--- symphony/src/Proethos2/CoreBundle/Controller/SecurityController.php
+++ symphony/src/Proethos2/CoreBundle/Controller/SecurityController.php
@@ -526,7 +526,7 @@ try {
 
     $send = $mailer->send($message);
 
-    /*
+    
 
     echo '<pre>';
 
@@ -588,7 +588,7 @@ try {
 
     echo "\n========================================\n";
     echo '</pre>';
-*/
+
     if ($send > 0) {
         $session->getFlashBag()->add(
             'success',
